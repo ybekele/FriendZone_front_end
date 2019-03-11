aa1b915ff2a58b0a5e411e81879bdab4beb58ba7
Yi <yi16@ualberta.ca>
2019-03-10 19:23:36 -0600

    ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Input, Button, Form, FormGroup, Label, Col, Spinner } from 'reactstrap';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Input, Button, Form, FormGroup, Label, Col, Spinner, Alert } from 'reactstrap';
 import './App.css';
 import classnames from 'classnames';
 import Homepage from './Homepage';
@@ -7,6 +7,7 @@ import Friends from './Friends';
 import Settings from './Settings';
 
 var host_url = 'http://127.0.0.1:8000';
+host_url = 'https://project-cmput404.herokuapp.com';
 var login_url = host_url+'/api/auth/login';
 var logout_url = host_url+'/api/auth/logout';
 var register_url = host_url+'/api/auth/register';
@@ -18,10 +19,9 @@ class App extends Component {
     this.toggle = this.toggle.bind(this);
     this.state = {
       activeTab: '1',
-      login: false,
+      login: true,
       token: 'null',
       signup: false,
-      username: 'null',
     };
   }
 
@@ -29,7 +29,13 @@ class App extends Component {
 
     // console.log(this.state);
 
-    var data = {"username": "yi", "email":"", "password":"1"};
+    var data = {
+      "username": document.getElementById("usernameText").value,
+      "email":"",
+      "password":document.getElementById("userPassword").value,
+    };
+
+    // var data = {"username": "yi", "email":"", "password":"1"};
 
     fetch(login_url, {
       method: 'POST', // or 'PUT'
@@ -44,6 +50,8 @@ class App extends Component {
       if (response.hasOwnProperty("token")){
         this.setState({login:true, token: response["token"]});
         console.log(this.state.token);
+      }else{
+        document.getElementById('alert').innerHTML = 'username & password do not match!!!';
       }
 
     })
@@ -59,7 +67,7 @@ class App extends Component {
         'Authorization': 'token '+this.state.token,
       }
     });
-    this.setState({login:false});
+    this.setState({login:false, signup:false});
   }
 
   tryregister(){
@@ -85,6 +93,8 @@ class App extends Component {
         if (response.hasOwnProperty("token")){
           this.setState({login:true, token: response["token"]});
           console.log(this.state.token);
+        } else{
+          document.getElementById('alert').innerHTML = JSON.stringify(response);
         }
 
       })
@@ -104,8 +114,8 @@ class App extends Component {
             return(
                 <center>
                   <Col sm="6">
-                    <Form  className='registerForm'>
-                      <Spinner type="grow" color="primary" />
+                  <h1>Sing Up</h1>
+                    <Form  className='loginForm'>
                       <FormGroup>
                         <Label for="usernameText">User name</Label>
                         <Input name="text" id="usernameText" placeholder="Enter your user name" />
@@ -118,8 +128,9 @@ class App extends Component {
                         <Label for="Text">Email</Label>
                         <Input name="text" id="emailText" placeholder="Enter your user email" />
                       </FormGroup>
-                      <Button onClick={()=> {this.setState({signup:false})}}>Go Back</Button>
-                      <Button onClick={()=> {this.tryregister();}}>Sign up</Button>
+                      <Button className='loginbuttons' onClick={()=> {this.setState({signup:false})}}>Go Back</Button>
+                      <Button className='loginbuttons' onClick={()=> {this.tryregister();}}>Sign up</Button>
+                      <Alert id='alert' color="info"></Alert>
                     </Form>
                   </Col>
                 </center>
@@ -138,8 +149,9 @@ class App extends Component {
                 <Label for="userPassword">Password</Label>
                 <Input type="password" name="password" id="userPassword" placeholder="Enter your password" />
               </FormGroup>
-              <Button onClick={()=> {this.trylogin();}}>Submit</Button>
-              <Button onClick={()=> {this.setState({signup: true});}}>Sign up</Button>
+              <Button className='loginbuttons' onClick={()=> {this.trylogin();}}>Login</Button>
+              <Button className='loginbuttons' onClick={()=> {this.setState({signup: true});}}>Sign up</Button>
+              <Alert id='alert' color="info"></Alert>
             </Form>
           </Col>
         </center>
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,30 +1,62 @@
 
 import React, { Component } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { InputGroup, InputGroupAddon, Input, Form, FormGroup, Collapse, Card, CardBody, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 
 class Homepage extends Component{
+
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { collapse: false };
+    }
+    
+    toggle() {
+    this.setState(state => ({ collapse: !state.collapse }));
+    }
+
     render(){
         return(
-            <Row>
-                <Col sm="12">
-                <h4>Homepage</h4>
+            <center>
+                <Button id='post' size='sm' color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Make Post!</Button>
+                
+                <Col sm="9">
+                    <Collapse isOpen={this.state.collapse}>
+                    <Form className="postForm">
+                        <FormGroup>
+                            <InputGroup>
+                                <Input placeholder="Image URL" />
+                                <InputGroupAddon addonType="append">
+                                <Input type="file" name="file" id="exampleFile" />
+                                </InputGroupAddon>
+                            </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                            <InputGroup>
+                                <Input placeholder="Tell us something!" />
+                                <InputGroupAddon addonType="append">
+                                <Button color="secondary">Post!</Button>
+                                </InputGroupAddon>
+                            </InputGroup> 
+                        </FormGroup>
+                    </Form>
+                    </Collapse>
+                    <h4>Your available posts:</h4>
+                    <Card body id = 'card'>
+                    <CardTitle>Example</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button onClick={
+                        ()=>{
+                            var card = document.getElementById('card');
+                            var item = document.createElement('h1');
+                            var content = document.createTextNode('content');
+                            item.appendChild(content);
+                            card.appendChild(item);
+                        }
+                    }>Go somewhere</Button>
+                    </Card>
                 </Col>
-                <Col sm="6">
-                <Card body id = 'card'>
-                  <CardTitle>Example</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                  <Button onClick={
-                      ()=>{
-                        var card = document.getElementById('card');
-                        var item = document.createElement('h1');
-                        var content = document.createTextNode('content');
-                        item.appendChild(content);
-                        card.appendChild(item);
-                      }
-                  }>Go somewhere</Button>
-                </Card>
-              </Col>
-            </Row>
+            </center>
+            
         );
         
     }
